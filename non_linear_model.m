5cda1cb928bfb9190cac65ce1a3d1740fb5b0740
Jason <jsun194113@gmail.com>
2022-07-27 15:53:07 +0800

    non_linear_model_2022.0727

---
+++ non_linear_model.m
@@ -0,0 +1,23 @@
+function [] = non_linear_model(r_r, D, D_tx, D_rx, emission_pt, receiver_pt)
+% diffusion coefficient
+diffusion_molecules = D;
+diffusion_tx = D_tx;
+diffusion_rx = D_rx;
+D1 = diffusion_molecules + diffusion_rx;
+D2 = diffusion_tx + diffusion_rx;
+
+% initial distance from Tx to Rx1 and Rx2
+tx_center = emission_pt;
+rx_center = receiver_pt;
+d_tx_rx_1 = norm(rx_center(1,:) - tx_center);
+d_tx_rx_2 = norm(rx_center(2,:) - tx_center);
+
+V_rx = 4*pi*r_r^3/3;
+
+load molecules_data.mat;
+X = time;
+y = nRx_1_avg;
+model_fun = @(b,tao)b(1)*V_rx.*erfc(d_tx_rx_1./((4*D2)^b(2).*(D1/D2.*tao).^b(3)))./(4*pi*D2*d_tx_rx_1);
+b_init = zeros(1,3);
+mdl = fitnlm(X,y,model_fun,b_init);
+end
