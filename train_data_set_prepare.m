f876bb08323022934ed8303256ab2bff9aa35e21
Jason <jsun194113@gmail.com>
2022-09-05 15:47:30 +0800

    避免相同的距离产生

--- train_data_set_prepare.m
+++ train_data_set_prepare.m
@@ -10,6 +10,9 @@ function [data_set] = train_data_set_prepare(num, node, r, n, NA, T, tau, diffus
 rand_distance = round(distance_low_bound + (distance_upper_bound - distance_low_bound) * rand(node, num));
 data_set = zeros(num,5);
 for i = 1 : num
+while(rand_distance(1, i) == rand_distance(2, i))
+    rand_distance(1:2, i) = round(distance_low_bound + (distance_upper_bound - distance_low_bound) * rand(node, 1));
+end
 fprintf(1, '\n############ distance:[%d, %d] ############\n',rand_distance(1, i), rand_distance(2, i));
 %% Experiment
 % 实验参数准备
@@ -17,7 +20,7 @@ emit_point = round(rand(1,2)*10);
 rx_1_point = rand_coordinate_generate(emit_point, rand_distance(1, i));
 rx_2_point = rand_coordinate_generate(emit_point, rand_distance(2, i));
 coordinate_vector = [[emit_point,0]; [rx_1_point,0]; [rx_2_point,0]];
-fprintf(1, '\n############ point:[%d; %d; %d] ############\n',coordinate_vector(1,1:end), coordinate_vector(2,1:end), coordinate_vector(3,1:end));
+fprintf(1, '\n############ point:[%d, %d, %d] ############\n',coordinate_vector(1,1:end), coordinate_vector(2,1:end), coordinate_vector(3,1:end));
 [Rx_1_estimate_coefficient, Rx_2_estimate_coefficient] = Example_runner_diffusion_rx(1, NA,coordinate_vector, r, diffusion_coefficient);
 
 molecule_to_allocated = sum(round(molecule_low_bound + (molecule_upper_bound - molecule_low_bound) * rand(node, 1)));
@@ -27,7 +30,7 @@ molecule_allocated = round(rand_distance(1 : node, i).' / s * molecule_to_alloca
 estimate_coefficient = [Rx_1_estimate_coefficient, Rx_2_estimate_coefficient];
 fprintf(1, '\n############ molecule_allocated:[%d, %d] ############\n',molecule_allocated(1), molecule_allocated(2));
 [local_optimum_molecule_allocated, local_optimum] = fminsearch_util(distance, n, molecule_allocated, r, diffusion_coefficient, estimate_coefficient, T, tau, molecule_low_bound, molecule_upper_bound);
-data_set(i,1:end) = [rand_distance(1 : node, i)', local_optimum_molecule_allocated, local_optimum];
+data_set(i, 1:end) = [rand_distance(1 : node, i)', local_optimum_molecule_allocated, local_optimum];
 end
 end
 
